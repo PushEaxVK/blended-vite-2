44eb499f987c42e82d2953bf9e0223784e9e2ea7
PushEax <pusheax.vk@gmail.com>
2025-04-27 11:22:05 +0300

    list item

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,6 +1,22 @@
-//import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
-const TodoListItem = () => {
-  return <h3>TodoListItem</h3>;
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import Text from '../Text/Text';
+import style from './TodoListItem.module.css';
+
+const TodoListItem = ({ item }) => {
+  return (
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        TODO #{item.id}
+      </Text>
+      <Text>{item.text}</Text>
+      <button className={style.deleteButton} type="button">
+        <RiDeleteBinLine size={24} />
+      </button>
+      <button className={style.editButton} type="button">
+        <RiEdit2Line size={24} />
+      </button>
+    </div>
+  );
 };
 
 export default TodoListItem;
