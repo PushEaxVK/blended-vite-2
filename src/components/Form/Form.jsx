6206000a62364f6bee1ddb6fd6a9488f32717ff8
PushEax <pusheax.vk@gmail.com>
2025-04-27 11:02:12 +0300

    Form complete

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,32 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
-const Form = () => {
-  return <h2>Form</h2>;
+const Form = ({ onSubmit }) => {
+  const formHandle = event => {
+    event.preventDefault();
+    const form = event.target;
+    const value = form.elements.search.value.trim();
+    if (value) {
+      onSubmit(value);
+    }
+    form.reset();
+  };
+
+  return (
+    <form className={style.form} onSubmit={formHandle}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
 
 const Todos = () => {
+  const [todoText, setTodoText] = useState('');
+
+  const addNewTodo = queryString => {
+    console.log(queryString);
+    setTodoText(queryString);
+  };
+
   return (
     <>
+      <Form onSubmit={addNewTodo} />
       <Text textAlign="center">There are no any todos ...</Text>
+      <Text textAlign="center">{todoText}</Text>
     </>
   );
 };
